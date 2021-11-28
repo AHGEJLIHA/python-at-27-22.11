55c13cbc2376752d85fb93de594c4d115d692aab
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-11-28 14:12:11 +0500

    2) Код переписан с использованием ThreadPoolExecutor 3) Измерено время работы при разном кол-ве воркеров (5, 10, 15)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import concurrent.futures
 import time
 from urllib.request import urlopen
-
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
@@ -19,23 +19,57 @@ for i in tqdm(range(100)):
             print(href, file=res)
 
 from urllib.request import Request, urlopen
+
 links = open('res.txt', encoding='utf8').read().split('\n')
 
-#Начало отсчета времени
-start_time = time.time()
-for url in links:
-    try:
-        request = Request(
-            url,
-            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) Gecko/20105107 Firefox/92.0'},
-        )
-        resp = urlopen(request, timeout=5)
-        code = resp.code
-        print(code)
-        resp.close()
-    except Exception as e:
-        print(url, e)
 
-#время синхронной проверки ссылок
-print("--- %s seconds ---" % (time.time() - start_time))
+def check_links(links):
+    for url in links:
+        try:
+            request = Request(
+                url,
+                headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) '
+                                       'Gecko/20105107 Firefox/92.0'},
+            )
+            resp = urlopen(request, timeout=5)
+            code = resp.code
+            print(code)
+            resp.close()
+        except Exception as e:
+            print(url, e)
+
+
+# Начало отсчета времени
+#start_time = time.time()
+#check_links()
+#end_time = time.time()
+# время синхронной проверки ссылок
+#print("--- %s seconds ---" % end_time - start_time)
+
+
+# Код с использованием ThreadPoolExecutor
+def split_to_parts(links, count):
+    intDiv, mod = (len(links) // count, len(links) % count)
+    return [links[min(i, mod):(i + 1) * intDiv + i * intDiv + min(i + 1, mod)]
+            for i in range(count)]
+
+
+worker_count = 10
+splited_links = split_to_parts(links, worker_count)
+dict_worker_links = {worker_id: splited_links[worker_id]
+                     for worker_id in range(worker_count)}
+
+for key in dict_worker_links:
+    print(key, ':', len(dict_worker_links[key]))
+
+async_start_time = time.time()
+with concurrent.futures.ThreadPoolExecutor(max_workers=worker_count) as executor:
+    dict_future_worker = {executor.submit(check_links, dict_worker_links[worker]):
+                              worker for worker in range(worker_count)}
+    for future in concurrent.futures.as_completed(dict_future_worker):
+        worker = dict_future_worker[future]
+        print({worker}, "finished checking ", {len(dict_worker_links[worker])}," URLs")
+async_end_time = time.time()
+result_time = async_end_time-async_start_time
+print(str(result_time) + " seconds")
 
